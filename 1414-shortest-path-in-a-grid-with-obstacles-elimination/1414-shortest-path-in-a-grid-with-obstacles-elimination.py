20e285fd3bcb4427445492ca1cae8f1907156830
sakshiWadaskar <144758641+sakshiwadaskar@users.noreply.github.com>
2025-10-21 16:34:34 -0400

    Time: 449 ms (27.23%), Space: 26.3 MB (8.13%) - LeetHub

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,29 +1,67 @@
+# import collections
+
+# class Solution:
+#     def shortestPath(self, grid: List[List[int]], k: int) -> int:
+#         directions = [(1,0), (-1,0), (0,1), (0,-1)]
+#         rows, cols = len(grid), len(grid[0])
+#         q = collections.deque([(0, 0, 0, k)])  # (steps, r, c, remaining eliminations)
+#         visited = set([(0, 0, k)])
+
+#         while q:
+#             steps, r, c, e = q.popleft()
+
+#             # Reached goal
+#             if (r, c) == (rows - 1, cols - 1):
+#                 return steps
+
+#             for dr, dc in directions:
+#                 nr, nc = r + dr, c + dc
+#                 if not (0 <= nr < rows and 0 <= nc < cols):
+#                     continue
+
+#                 newE = e - grid[nr][nc]  # use one elimination if obstacle
+#                 if newE < 0:
+#                     continue  # can’t pass obstacle
+#                 if (nr, nc, newE) not in visited:
+#                     visited.add((nr, nc, newE))
+#                     q.append((steps + 1, nr, nc, newE))
+
+#         return -1
+
+
+
+
+
+
+
+
+
+
 import collections
 
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        visited = set([0,0,0])       #r, c, e
+        directions = [[1,0], [-1,0], [0, 1], [0, -1]]
         rows, cols = len(grid), len(grid[0])
-        q = collections.deque([(0, 0, 0, k)])  # (steps, r, c, remaining eliminations)
-        visited = set([(0, 0, k)])
-
+        q = collections.deque([(0, 0, 0, k)])
+        steps = 0
         while q:
             steps, r, c, e = q.popleft()
 
-            # Reached goal
+            visited.add((r, c, e))
+
             if (r, c) == (rows - 1, cols - 1):
                 return steps
-
+            
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
                 if not (0 <= nr < rows and 0 <= nc < cols):
                     continue
-
-                newE = e - grid[nr][nc]  # use one elimination if obstacle
+                newE = e - grid[nr][nc]
                 if newE < 0:
-                    continue  # can’t pass obstacle
+                    continue
                 if (nr, nc, newE) not in visited:
                     visited.add((nr, nc, newE))
                     q.append((steps + 1, nr, nc, newE))
-
         return -1

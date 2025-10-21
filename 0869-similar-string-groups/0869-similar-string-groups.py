c4c9ccfd4949cb0a471d31e57694503e53dbc5dc
sakshiWadaskar <144758641+sakshiwadaskar@users.noreply.github.com>
2025-10-21 16:12:17 -0400

    Time: 1122 ms (37.95%), Space: 18.9 MB (6.25%) - LeetHub

--- 0869-similar-string-groups/0869-similar-string-groups.py
+++ 0869-similar-string-groups/0869-similar-string-groups.py
@@ -1,15 +1,16 @@
 class Solution:
     def numSimilarGroups(self, strs: List[str]) -> int:
-        visited = [False] * len(strs)
         groups = 0
+        visited = [False] * len(strs)
 
         def dfs(i):
             visited[i] = True
-            
+
             for j in range(len(strs)):
                 if visited[j]: continue
                 if isSimilar(strs[i], strs[j]):
                     dfs(j)
+
         def isSimilar(s1, s2):
             count = 0
             for i in range(len(s1)):
@@ -17,11 +18,10 @@ class Solution:
                     count += 1
             return count == 2 or count == 0
 
+
         for i in range(len(strs)):
             if not visited[i]:
                 dfs(i)
                 groups += 1
         return groups
-
-
         

daaec288caa238a1a789867eaf715093628576f1
sakshiWadaskar <144758641+sakshiwadaskar@users.noreply.github.com>
2026-09-13 11:57:42 -0400

    Time: 6 ms (58.56%), Space: 19.7 MB (14.17%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,26 +1,25 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
         candidates.sort()
-        res, path = [], []
 
-        def dfs(start, total, path):
-
-            if target == total:
+        def backtrack(start: int, total: int, path: List[int]):
+            if total == target:
                 res.append(path.copy())
+                return
 
             for i in range(start, len(candidates)):
-
-                if candidates[i] + total > target:
+                # Pruning: sorted array means all subsequent additions will also exceed target
+                if total + candidates[i] > target:
                     break
 
                 # Skip duplicate numbers at the same decision level
                 if i > start and candidates[i] == candidates[i - 1]:
                     continue
-                
+
                 path.append(candidates[i])
-                dfs(i + 1, candidates[i] + total, path)
+                backtrack(i + 1, total + candidates[i], path)
                 path.pop()
 
-        dfs(0, 0, path)
+        backtrack(0, 0, [])
         return res
-        
